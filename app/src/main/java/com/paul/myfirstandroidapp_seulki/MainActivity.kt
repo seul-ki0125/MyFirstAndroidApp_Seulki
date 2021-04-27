53749e4364a488df27e1ec8083a71ee18e8560e9
seulki <seulkiyoo89@gmail.com>
2021-04-27 14:00:02 +0900

    Initial commit

---
+++ app/src/main/java/com/paul/myfirstandroidapp_seulki/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.paul.myfirstandroidapp_seulki
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
